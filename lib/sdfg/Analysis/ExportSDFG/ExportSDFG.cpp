06db5dbe0dae19772462eb60bec8a8a9a3e68ce3
Lukas Truemper <lukas.truemper@outlook.de>
2025-07-13 21:36:12 +0200

    enables rtti and exceptions for symengine/sdfglib

--- lib/sdfg/Analysis/ExportSDFG/ExportSDFG.cpp
+++ lib/sdfg/Analysis/ExportSDFG/ExportSDFG.cpp
@@ -8,43 +8,83 @@
 #include "mlir/IR/BuiltinAttributes.h"
 #include "llvm/Support/raw_ostream.h"
 
-using namespace mlir;
-using namespace mlir::sdfg;
-using namespace mlir::sdfg::analysis;
+#include "sdfg/builder/structured_sdfg_builder.h"
+#include "sdfg/serializer/json_serializer.h"
 
 namespace {
 
-struct ExportSDFGPass : public ExportSDFGPassBase<ExportSDFGPass> {
+struct ExportSDFGPass : public mlir::sdfg::analysis::ExportSDFGPassBase<ExportSDFGPass> {
+  void visit_alloca(mlir::sdfg::AllocaOp allocaOp) {
+    llvm::outs() << "AllocaOp\n";
+  }
+
+  void visit_library_node(mlir::sdfg::LibraryNodeOp libraryNodeOp) {
+    llvm::outs() << "LibraryNode\n";
+  }
+
+  void visit_return(mlir::sdfg::ReturnOp returnOp) {
+    llvm::outs() << "ReturnOp\n";
+  }
+
+  std::unique_ptr<sdfg::StructuredSDFG> simplify(std::unique_ptr<sdfg::StructuredSDFG>& sdfg) {
+    // TODO: Implement
+    return std::move(sdfg);
+  }
+
   void runOnOperation() override {
-    ModuleOp module = getOperation();
+    mlir::ModuleOp module = getOperation();
 
-    module.walk([&](SDFGNode sdfgNode) {
-      llvm::outs() << "=== SDFG Node: ";
-      if (auto symAttr = sdfgNode->getAttrOfType<StringAttr>("sym_name"))
-        llvm::outs() << symAttr.getValue();
+    module.walk([&](mlir::sdfg::SDFGNode sdfgNode) {
+      std::string sdfgName;
+      if (auto symAttr = sdfgNode->getAttrOfType<mlir::StringAttr>("sym_name"))
+        sdfgName = symAttr.getValue();
       else
-        llvm::outs() << "<unnamed>";
-      llvm::outs() << " ===\n";
+        sdfgName = "_unnamed_";
 
-      // Fetch function type held in attribute.
-      auto funcTypeAttr = sdfgNode->getAttrOfType<TypeAttr>("function_type");
+      sdfg::builder::StructuredSDFGBuilder builder(sdfgName, sdfg::FunctionType_CPU);
+
+      // Visit arguments of the SDFG function.
+      auto funcTypeAttr = sdfgNode->getAttrOfType<mlir::TypeAttr>("function_type");
       if (funcTypeAttr) {
-        auto funcType = funcTypeAttr.getValue().cast<FunctionType>();
-        llvm::outs() << "  Num Inputs : " << funcType.getNumInputs() << "\n";
-        llvm::outs() << "  Num Results: " << funcType.getNumResults() << "\n";
+        auto funcType = funcTypeAttr.getValue().cast<mlir::FunctionType>();
+        for (auto arg : funcType.getInputs()) {
+          // Add name and type to sdfg
+          // builder.add_container(name, type, true);
+        }
       }
 
-      // Count operations inside the SDFG body region.
-      size_t opCount = 0;
-      sdfgNode.getBody().walk([&](Operation *innerOp) {
-        // Skip the region terminator
-        if (isa<ReturnOp>(innerOp))
-          return;
-        ++opCount;
+      // Visit all operations in the SDFG body region.
+      sdfgNode.getBody().walk([&](mlir::Operation *innerOp) {
+        if (auto allocaOp = dyn_cast<mlir::sdfg::AllocaOp>(innerOp)) {
+          visit_alloca(allocaOp);
+        } else if (auto libraryNodeOp = dyn_cast<mlir::sdfg::LibraryNodeOp>(innerOp)) {
+          visit_library_node(libraryNodeOp);
+        } else if (auto returnOp = dyn_cast<mlir::sdfg::ReturnOp>(innerOp)) {
+          visit_return(returnOp);
+        } else {
+          throw std::runtime_error("Unsupported operation: " +
+                                   innerOp->getName().getStringRef().str());
+        }
       });
-      llvm::outs() << "  Contained ops: " << opCount << "\n";
 
-      // TODO: Extend to gather and print more metadata.
+      // Finish SDFG
+      auto sdfg = builder.move();
+
+      // Simplify SDFG
+      sdfg = simplify(sdfg);
+
+      // Serialize SDFG to JSON
+      sdfg::serializer::JSONSerializer serializer;
+      auto j = serializer.serialize(sdfg);
+      std::filesystem::path sdfgPath = sdfgName + ".json";
+
+      std::ofstream ofs(sdfgPath);
+      if (!ofs.is_open()) {
+          throw std::runtime_error("Failed to open file: " + sdfgPath.string());
+      }
+      ofs << j.dump(2);
+      ofs.close();
+
     });
   }
 };
